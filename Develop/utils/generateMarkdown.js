3783819534bdb75569a6873ffc76a57a9f9bed99
Nafisa Mohamed <nafisa.am91@gmail.com>
2022-11-12 02:14:16 +0000

    license section of readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,10 +12,19 @@ const generateMarkdown = (data) => {
       licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
       break;
     case "None":
+      licenseBadge = "";
       break;
   }
 };
-
+//  A function that returns the license section of README
+// If there is no license, return an empty string
+const renderLicenseSection = (license) => {
+  if (license === "None") {
+    return "No";
+  } else {
+    return license;
+  }
+};
 //  A function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
