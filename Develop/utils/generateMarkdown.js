76c7cc3cb4777c2e37f4a676f5a92561cd0e25b3
Nafisa Mohamed <nafisa.am91@gmail.com>
2022-11-12 02:06:43 +0000

    license badge handling

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,20 @@
 //  A function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-//  A function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-//  A function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const generateMarkdown = (data) => {
+  switch (data.license) {
+    case "MIT":
+      licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      break;
+    case "Apache 2.0":
+      licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-red.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    case "GNU GPLv3.0":
+      licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+      break;
+    case "None":
+      break;
+  }
+};
 
 //  A function to generate markdown for README
 function generateMarkdown(data) {
